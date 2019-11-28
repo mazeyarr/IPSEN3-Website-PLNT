572daf9460a4b2b4755e61086f61ba0b909fbd3c
Mazeyar Rezaei <mazeyarr@gmail.com>
2019-11-28 18:22:57 +0100

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './services/config/config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'website';
+
+  constructor(private configService: ConfigService) {
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.configService.getConfig());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NavBarBrandComponent } from './nav-bar-brand/nav-bar-brand.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    NavBarBrandComponent
+    NavBarBrandComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot()
