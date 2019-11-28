94421933352b0c1ef848c9781c327362f1314993
Mazeyar Rezaei <mazeyarr@gmail.com>
2019-11-28 13:04:56 +0100

    navbar with login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarBrandComponent } from './nav-bar-brand/nav-bar-brand.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    NavBarBrandComponent
   ],
   imports: [
     BrowserModule,
