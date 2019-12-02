eaa01afa82249af364c7881af6084be226116922
Mazeyar Rezaei <mazeyarr@gmail.com>
2019-12-02 16:03:19 +0100

    added methods to api service

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface IApiOptions {
+  auth?: boolean;
+  endpoint?: string;
+  body?: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   private static readonly baseUrl = 'http://localhost:9000';
   private static readonly prefix = '/api';
 
-  static get() {
-
+  get(options: IApiOptions) {
+    return this.http.get(this.getApiUrl() + options.endpoint, options.body);
   }
 
-  static post() {
-
+  post(options: IApiOptions) {
+    return this.http.post(this.getApiUrl() + options.endpoint, options.body);
   }
 
-  static put() {
-
+  put(options: IApiOptions) {
+    return this.http.put(this.getApiUrl() + options.endpoint, options.body);
   }
 
   getApiUrl = (): string => ApiService.baseUrl + ApiService.prefix;
