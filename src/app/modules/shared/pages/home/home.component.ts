92a680b6796db19834394a0abd933f61248bb747
Mazeyar Rezaei <mazeyarr@gmail.com>
2019-12-05 11:50:31 +0100

    modal example

--- src/app/modules/shared/pages/home/home.component.ts
+++ src/app/modules/shared/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { slideInAnimation } from '../../../../app.route-animations';
+import { MDBModalRef, MDBModalService } from 'angular-bootstrap-md';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +10,15 @@ import { slideInAnimation } from '../../../../app.route-animations';
   animations: [ slideInAnimation ]
 })
 export class HomeComponent implements OnInit {
+  modalRef: MDBModalRef;
 
-  constructor() { }
+  constructor(private modalService: MDBModalService) {}
 
-  ngOnInit() {
+  openModal() {
+    this.modalRef = this.modalService.show(ModalComponent);
+  }
+
+  ngOnInit(): void {
   }
 
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { RouterModule } from '@angular/router';
 import { sharedRoutes } from './shared.routes';
-
-
+import { ModalComponent } from './components/modal/modal.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ModalComponent],
   imports: [
     RouterModule.forRoot(sharedRoutes),
-    CommonModule
-  ]
+    CommonModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  entryComponents: [ModalComponent],
+  providers: []
 })
-export class SharedModule { }
+export class SharedModule {
+}
