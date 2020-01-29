a6bf8f644771698607851906566662b632534c0c
Joeri van Duijkeren <joerivanduijkeren@gmail.com>
2020-01-29 12:07:44 +0100

    frotnend project uploaden(beschrijvingen,tags etc)

--- src/app/modules/project/components/create-project-data/create-project-data.component.spec.ts
+++ src/app/modules/project/components/create-project-data/create-project-data.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+gimport { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateProjectDataComponent } from './create-project-data.component';
 
